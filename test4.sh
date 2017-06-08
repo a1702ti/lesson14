20873c4a32a20c84bea4f8eaee65c28ec0a2e8d4
testuser1 <a1702ti@aiit.ac.jp>
2017-06-09 01:39:54 +0900

    Bug Fix

--- test4.sh
+++ test4.sh
@@ -13,11 +13,11 @@
 #########
 
     echo "Input error. Please input two numbers." > $tmp-ans
-    ./17745102 100 > $tmp-err && ERROR_EXIT "TEST4-1"
+    ./17745102 2> $tmp-err && ERROR_EXIT "TEST4-1"
     diff $tmp-ans $tmp-err || ERROR_EXIT "TEST4-2"
 
     echo "input 100"
-    cat $tmp-out
+    cat $tmp-err
     echo TEST4 OK
     rm -f $tmp-*
     exit 0
